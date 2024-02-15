cae4c55de79a412395ba72c409a5a2c129f41da6
GaripovDaniil <Garipov.Daniil@urfu.me>
2024-02-15 15:35:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var count: Int = 0
+    private lateinit var countText: TextView
+    private lateinit var calculateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        countText = findViewById(R.id.count)
+        calculateButton = findViewById(R.id.calculate_button)
+        calculateButton.setOnClickListener{ calculateSquare() }
+        Log.i("MainActivity", "onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count", count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("count") + 1
+        countText.text = count.toString()
+    }
+
+    private fun calculateSquare() {
+        val squareIntent = Intent(this, SquareActivity::class.java)
+        squareIntent.putExtra(SquareActivity.COUNT, count)
+        startActivity(squareIntent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy")
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SquareActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class SquareActivity : AppCompatActivity() {
+
+    private lateinit var squareCountText: TextView
+
+    companion object {
+        const val COUNT = "count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square)
+        squareCountText = findViewById(R.id.square_count)
+        showSquareCount()
+        Log.i("SquareActivity", "onCreate")
+    }
+
+    private fun showSquareCount() {
+        val count = intent.getIntExtra(COUNT, 0)
+        squareCountText.text = (count * count).toString()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("SquareActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("SquareActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("SquareActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("SquareActivity", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("SquareActivity", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("SquareActivity", "onDestroy")
+    }
+}
